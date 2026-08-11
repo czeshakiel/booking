62e0d4fe17ad3baaeaf27ec0455260ff122b5e9b
shakiel17 <hisoka.aboy@gmail.com>
2026-08-11 10:25:39 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$host= $_SERVER['HTTP_HOST'];
-$config['base_url'] = 'http://'.$host.'/e-learning';
+$host = $_SERVER['HTTP_HOST'];
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off'){    
+		$uri = 'https://'.$host;
+	} else {
+		$uri = 'http://'.$host;
+	}
+$config['base_url'] = $uri.'/booking';
 
 /*
 |--------------------------------------------------------------------------
